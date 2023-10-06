ec52344bc8a28c173b2ea01356a3310927848f5e
lhyodo <leo.hyodo@outlook.com>
2023-10-06 14:53:40 -0700

    pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(none, ntwo) {
 
+  if (none < 0 || ntwo < 0) {
+    return "ERROR";
+  }
+  if (typeof none != 'number' || typeof ntwo != 'number') {
+    return "ERROR";
+  }
+
+
+  let min = 0;
+  let max = 0;
+  if (none > ntwo) {
+    max = none;
+    min = ntwo;
+  } else {
+    max = ntwo;
+    min = none;
+  }
+
+  let res = 0;
+  for (let i = min; i <= max; ++i) {
+    res += i;
+  }
+  return res;
 };
 
 // Do not edit below this line
