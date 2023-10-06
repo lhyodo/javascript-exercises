4863060896e5604a124c69296bfc0da8d92ad863
lhyodo <leo.hyodo@outlook.com>
2023-10-06 14:28:29 -0700

    pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, foo, goo, hoo, joo, loo) {
 
+
+  let res = [];
+
+  for (let i = 0; i < arr.length; ++i) {
+    if (arr[i] === foo || arr[i] === goo || arr[i] === hoo || arr[i] === joo || arr[i] === loo) {
+      // do nothing
+    }
+    else {
+      res.push(arr[i]);
+    }
+  }
+  return res;
 };
 
 // Do not edit below this line
