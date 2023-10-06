b13e2c820f3aa26ad2b66eebd25b52a50f3dfbf8
lhyodo <leo.hyodo@outlook.com>
2023-10-06 14:10:41 -0700

    pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(foo, n) {
+  if (n < 0) {
+    return "ERROR";
+  }
+  let goo = '';
+  for (let i = 0; i < n; ++i) {
+    goo += foo;
+  }
+  return goo;
 };
 
 // Do not edit below this line
