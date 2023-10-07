5e337944a2b2db9d4612b681fadf3681ca55bfcb
lhyodo <leo.hyodo@outlook.com>
2023-10-06 19:17:51 -0700

    pass

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(nOne, nTwo) {
+	let res = nOne + nTwo;
+  return res;
 };
 
-const subtract = function() {
-	
+const subtract = function(nOne, nTwo) {
+	let res = nOne - nTwo;
+  return res;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let res = 0;
+	for (let i = 0; i < arr.length; ++i) {
+    res += arr[i];
+  }
+  return res;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (arr.length == 1) {
+    return arr[0];
+  }
+  let res = arr[0];
+  for (let i = 1; i < arr.length; ++i) {
+    res *= arr[i];
+  }
+  return res;
 };
 
-const power = function() {
-	
+const power = function(nOne, nTwo) {
+	let res = nOne ** nTwo;
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(nOne) {
+
+  if (nOne == 0) {
+    return 1;
+  }
+
+
+	let res = nOne;
+  for (let i = nOne - 1; i > 0; --i) {
+    res *= i;
+  }
+  return res;
 };
 
 // Do not edit below this line
