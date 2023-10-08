47f986edb48fb786a1e6c2d51f84badd78d03d0e
lhyodo <leo.hyodo@outlook.com>
2023-10-07 18:02:08 -0700

    pass

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  // 1 1 2 3 5 8 13 21 ...
+
+  if (n <= 0) {
+    return "OOPS";
+  }
+
+  const res = [];
+  res[0] = 1;
+  res[1] = 1;
+  for (let i = 2; i < n; ++i) {
+    res[i] = res[i - 1] + res[i - 2];
+  }
+  return res[n - 1];
 
 };
 
