063d984e2aaa5cfa0dcfa6c2e2abf25c92abe2e2
lhyodo <leo.hyodo@outlook.com>
2023-10-07 18:41:21 -0700

    pass

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+  const date = new Date();
+  const currentYear = date.getFullYear();
+
+  let oldest = people[0];
+  let oldestAge = 0;
+
+  for (let i = 0; i < people.length; ++i) {
+
+    let age = 0;
+    if (people[i].yearOfDeath == null) {
+      age = currentYear - people[i].yearOfBirth;
+    } else {
+      age = people[i].yearOfDeath - people[i].yearOfBirth;
+    }
+    
+    if (age > oldestAge) {
+      oldestAge = age;
+      oldest = people[i];
+    }
+
+  }
+  return oldest;
 
 };
 
